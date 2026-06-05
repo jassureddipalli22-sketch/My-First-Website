685a116775990541ff73be3b22bb10bf0460bd41
jaswanthi <jassureddipalli22@gmail.com>
2026-06-05 16:17:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const nav = document.getElementById('navbar');
+
+
+if(bar) {
+    bar.addEventListener('click',()=)
+}
